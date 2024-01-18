f4bb25df568b9825d957417c101fd23c131135f3
harsha <harsha200124@gmail.com>
2024-01-18 22:44:17 +0530

    style

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import Header from './Header'; 
 
 class App extends Component{ 
   render() {
   return (
-    <div >
-      Phone Directory 
-      
-    </div>
+    <div>
+
+    <Header/>
+ 
+  <button>Add </button>
+  <div>
+    <span>Name</span><br/>
+    <span>Phone number </span>
+  </div>
+  
+  <label htmlFor='name'>Name:</label>
+  <input type="text" placeholder="type here" defaultValue="Shilp" id='name' />*/
+  
+
+        </div>
   );
 }
 }
-
-
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+
+const Header =()=>{
+    return(
+        <div className="header">
+            phone directory 
+        </div>
+    )
+}
+export default Header;
